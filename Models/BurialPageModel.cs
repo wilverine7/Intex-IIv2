3b805dd6e32810cb03356ab7d8d900fcde18aef5
Sidney Eaton <sideaton@byu.edu>
2023-04-13 22:52:08 -0600

    filtering!

--- Models/BurialPageModel.cs
+++ Models/BurialPageModel.cs
@@ -10,6 +10,17 @@ namespace Mummies.Models
 		public string BurialId { get; set; }
 		public string BurialDepth { get; set; }
 		public string Preservation { get; set; }
+		public string Haircolor { get; set; }
+		public string Age { get; set; }
+		public string HeadDirection { get; set; }
+		public string FaceBundles { get; set; }
+		public string SquareNorthSouth { get; set; }
+		public string NorthSouth { get; set; }
+		public string SquareEastWest { get; set; }
+		public string EastWest { get; set; }
+		public string Area { get; set; }
+		public string BurialNumber { get; set; }
+
 
     }
 }
